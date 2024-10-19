df5b5ec303b89fc302420cf28beddfdd633b34a9
Timotej Bernat <tibe3324@colorado.edu>
2024-10-18 18:14:32 -0600

    Fixed relative imports to utils within src.init_signac.py

--- init_project.sh
+++ init_project.sh
@@ -15,12 +15,12 @@ FLAG_PROD="--production"
 if [[ $runlevel == $FLAG_TEST ]]; then
     echo "Initializing test project..."
     projdir="${projname}_test"
-    python src/init_signac.py -mdat $DATAPATH -num 12 --random --project-name $projdir -od $OUTDIR
+    python -m src.init_signac -mdat $DATAPATH -num 12 --random --project-name $projdir -od $OUTDIR
     echo "Project directory '${projdir}' created"
 elif [[ $runlevel == $FLAG_PROD ]]; then
     echo "Initializing production-scale project..."
     projdir="${projname}_production"
-    python src/init_signac.py -mdat $DATAPATH --project-name $projdir -od $OUTDIR
+    python -m src.init_signac -mdat $DATAPATH --project-name $projdir -od $OUTDIR
     echo "Project directory '${projdir}' created"
 else
     echo "Invalid flag '$runlevel'; select either '$FLAG_TEST' or '$FLAG_PROD'"
--- src/init_signac.py
+++ src/init_signac.py
@@ -16,11 +16,11 @@ from signac import init_project
 from src.parameters import ParametersSwept, PARAMS_SWEPT_PATH 
 from src.parameters import ParametersConfig, PARAMS_CONFIG_PATH
 
-from utils.filelib import validate_file_path
-from utils.dataIO import read_monomer_data
-from utils.datafmt import parse_field_names_and_roles
+from src.utils.filelib import validate_file_path
+from src.utils.dataIO import read_monomer_data
+from src.utils.datafmt import parse_field_names_and_roles
 
-from utils.containers import cartesian_grid
+from src.utils.containers import cartesian_grid
 
 
 # Helper functions
