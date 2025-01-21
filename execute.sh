4bc064d4b065f18f5604a6ce43a97185b35c0cbf
Timotej Bernat <tibe3324@colorado.edu>
2025-01-21 16:00:48 -0700

    Fixed projdir misquote, added bundling to scheduler submit command

--- execute.sh
+++ execute.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-projdir=""${1:-"polyID_test"}"
+projdir="${1:-"polyID_test"}"
 
 # Get current status of project
 # python src/project.py -path $projdir status --detailed --pretty
 
 # Send all project jobs to the Cluster
 # python src/project.py -path $projdir submit --parallel --partition blanca-shirts -o everything --job-output dumps.out # --pretend # Shirts Blanca nodes
-python src/project.py -path polyID_test submit --parallel -o everything --partition amilan --account ucb576_asc1 --job-output dumps.out # Alpine allocation
+python src/project.py -path $projdir submit --parallel --bundle 10 -o everything --partition amilan --account ucb576_asc1 --job-output polyid_build.out # Alpine allocation
