069316f4637e4aa525894ebb4b011c5f03f5ef99
Timotej Bernat <tibe3324@colorado.edu>
2025-03-19 16:02:12 -0600

    Decreased scheduler bundle size from 10 to 6

--- execute.sh
+++ execute.sh
@@ -7,4 +7,4 @@ projdir="${1:-"polyID_test"}"
 
 # Send all project jobs to the Cluster
 # python src/project.py -path $projdir submit --parallel --partition blanca-shirts -o everything --job-output dumps.out # --pretend # Shirts Blanca nodes
-python src/project.py -path $projdir submit --parallel --bundle 10 -o everything --partition amilan --account ucb576_asc1 --job-output polyid_build.out # Alpine allocation
+python src/project.py -path $projdir submit --parallel --bundle 6 -o everything --partition amilan --account ucb576_asc1 --job-output polyid_build.out # Alpine allocation
