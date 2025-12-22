80b1576e4652b92e36e4c5410a2efec2786d9ff6
Timotej Bernat <tibe3324@colorado.edu>
2025-12-22 15:17:38 -0700

    Updated Ascent allocation for 2025-26

--- execute.sh
+++ execute.sh
@@ -8,4 +8,4 @@ logfile="${2:-"polyid_build.out"}"
 
 # Send all project jobs to the Cluster
 # python -m src.project -path $projdir submit --parallel --partition blanca-shirts -o everything --job-output dumps.out # --pretend # Shirts Blanca nodes
-python -m src.project -path $projdir submit --parallel --bundle 6 -o everything --partition amilan --account ucb576_asc1 --job-output $logfile # Alpine allocation
+python -m src.project -path $projdir submit --parallel --bundle 6 -o everything --partition amilan --account ucb500_asc2 --job-output $logfile # Alpine allocation
