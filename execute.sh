0615aae8562dc1665ebe20625dc19a57db18552c
Timotej Bernat <tibe3324@colorado.edu>
2026-01-05 22:37:04 -0700

    Indented submission commands to aid config and readability

--- execute.sh
+++ execute.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 projdir="${1:-"polyID_test"}"
-logfile="${2:-"polyid_build.out"}"
+batchsize="${2:-6}"
+logfile="${3:-${projdir}_build.out}"
 
 # Get current status of project
 # python -m src.project -path $projdir status --detailed --pretty
 
 # Send all project jobs to the Cluster
 # python -m src.project -path $projdir submit --parallel --partition blanca-shirts -o everything --job-output dumps.out # --pretend # Shirts Blanca nodes
-python -m src.project -path $projdir submit --parallel --bundle 6 -o everything --partition amilan --account ucb500_asc2 --job-output $logfile # Alpine allocation
+python -m src.project \
+    -path $projdir \
+    submit --parallel \
+    --bundle $batchsize \
+    --operation everything \
+    --job-output $logfile \
+    --partition amilan \
+    --account ucb500_asc2
--- status.sh
+++ status.sh
@@ -4,6 +4,6 @@ projdir="${1:-"polyID_test"}"
 rxnpath="${2:-"src/reactions/rxns_polyID.json"}"
 
 python -m src.project \
-    -path "$projdir" \
-    -rxns "$rxnpath" \
+    -path $projdir \
+    -rxns $rxnpath \
     status \
