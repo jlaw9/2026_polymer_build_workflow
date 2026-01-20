ef45789bc236930f0472a425f0fa6d90a62e4248
timbernat <tibe3324@colorado.edu>
2026-01-19 18:39:05 -0700

    Added shared path for custom local SMIRNOFF forcefield files

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,11 @@ __email__ = 'timotej.bernat@colorado.edu'
 
 from pathlib import Path
 _parent_dir = Path(__file__).parent.resolve()
+
+try:
+    from .utils.forcefields import FF_DIRNAME
+except ImportError:
+    from utils.forcefields import FF_DIRNAME
+
+FF_DIR = _parent_dir / FF_DIRNAME
+FF_DIR.mkdir(exist_ok=True)
